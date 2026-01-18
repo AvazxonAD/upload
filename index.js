fa89734f664035e642065818e7f27acb7e4f4d97
AvazxonAD <asqarovavazxon77@gmail.com>
2026-01-18 22:42:49 +0500

    jurnal

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.use("/", express.static("public")).get("*", (req, res) => {
-  return res.sendFile("/home/erkatoyu/apps/erkatoy/front/admin/public/index.html");
+  return res.sendFile("/home/erkatoyu/apps/erkatoy/front/user/public/index.html");
 });
 
 app.listen(5173, () => {
